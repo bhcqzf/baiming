43ba209870cc2ec1a5f228777524af0cf6d7acf7
bai4246464@gmail.com <Bai:000123>
2019-12-10 22:57:48 +0800

    1

--- 笔趣阁.py
+++ 笔趣阁.py
@@ -1,20 +1,23 @@
 def download_one(url):
     import requests
     from bs4 import BeautifulSoup
-    r=requests.get(url)
-    r.encoding=r.apparent_encoding
-    demo=r.text
+    r = requests.get(url)
+    r.encoding = r.apparent_encoding
+    demo = r.text
     # print(r.text)
-    soup=BeautifulSoup(demo,'html.parser')
+    soup = BeautifulSoup(demo, 'lxml')
     # print(soup.prettify())
-    title=soup.find('div',class_='bookname').h1.text
+    booktitle = soup.select('#wrapper > div.content_read > div > div.con_top > a')[-1].text
+    print(booktitle)
+    title = soup.find('div', class_='bookname').h1.text
     print(title)
-    txts=soup.find_all('div',id="content")
+    txts = soup.find_all('div', id="content")
     for txt in txts:
-        txt1=txt.text
-        with open(title+'.txt','w',encoding='utf-8') as f:
-            f.write(title)
-            f.write(txt1)
+        txt1 = txt.text
+        with open(booktitle + '.txt', 'a', encoding='utf-8') as f:
+            print(txt1)
+            f.write(title + '\n')
+            f.write(txt1 + '\n' + '\n')
     pass
 
 '''
@@ -39,17 +42,20 @@ for url in urls:
 '''
 
 
-
-import requests
-from bs4 import BeautifulSoup
-header={'User-Agent': 'Mozilla/5.0'}
-r=requests.get('http://www.xbiquge.la/10/10489/',headers=header)
-r.encoding=r.apparent_encoding
-soup=BeautifulSoup(r.text,'lxml')
-# print(soup.prettify())
-urls=soup.select("#list > dl > dd > a")
-for url1 in urls:
-    # print(url1)
-    url2=url1.get('href')
-    urlzz=('http://www.xbiquge.la'+url2)
-    download_one(urlzz)
+def download_one_book(url):
+    import requests
+    from bs4 import BeautifulSoup
+    header={'User-Agent': 'Mozilla/5.0'}
+    r=requests.get('url',headers=header)
+    r.encoding=r.apparent_encoding
+    soup=BeautifulSoup(r.text,'lxml')
+    # print(soup.prettify())
+    urls=soup.select("#list > dl > dd > a")
+    for url1 in urls:
+        # print(url1)
+        url2=url1.get('href')
+        urlzz=('http://www.xbiquge.la'+url2)
+        download_one(urlzz)
+    pass
+url=input('请输入要下载小说的目录网址')
+download_one_book(url)
