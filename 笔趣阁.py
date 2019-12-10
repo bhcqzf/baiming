1845233472c372c7b134f15d90f862e54c8dc90e
bai4246464@gmail.com <Bai:000123>
2019-12-10 23:31:25 +0800

    新笔趣阁小说网爬虫 网址：http://www.xbiquge.la/

--- 笔趣阁.py
+++ 笔趣阁.py
@@ -8,14 +8,14 @@ def download_one(url):
     soup = BeautifulSoup(demo, 'lxml')
     # print(soup.prettify())
     booktitle = soup.select('#wrapper > div.content_read > div > div.con_top > a')[-1].text
-    print(booktitle)
+    # print(booktitle)
     title = soup.find('div', class_='bookname').h1.text
-    print(title)
+    print('正在下载  '+booktitle+title)
     txts = soup.find_all('div', id="content")
     for txt in txts:
         txt1 = txt.text
         with open(booktitle + '.txt', 'a', encoding='utf-8') as f:
-            print(txt1)
+            # print(txt1)
             f.write(title + '\n')
             f.write(txt1 + '\n' + '\n')
     pass
@@ -46,7 +46,7 @@ def download_one_book(url):
     import requests
     from bs4 import BeautifulSoup
     header={'User-Agent': 'Mozilla/5.0'}
-    r=requests.get('url',headers=header)
+    r=requests.get(url,headers=header)
     r.encoding=r.apparent_encoding
     soup=BeautifulSoup(r.text,'lxml')
     # print(soup.prettify())
@@ -57,5 +57,9 @@ def download_one_book(url):
         urlzz=('http://www.xbiquge.la'+url2)
         download_one(urlzz)
     pass
-url=input('请输入要下载小说的目录网址')
-download_one_book(url)
+
+# url=input('请输入要下载小说的目录网址:')
+# url1=str(url)
+# download_one_book(url1)
+# print(url1)
+download_one_book('http://www.xbiquge.la/1/1690/')   //此处输入要下载小说目录的链接
