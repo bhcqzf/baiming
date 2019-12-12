e223c9d6854c11b7aea7ab4121d14f7d391ce027
bai4246464@gmail.com <Bai:000123>
2019-12-12 11:17:31 +0800

    优化代码

--- 书趣阁小说爬虫.py
+++ 书趣阁小说爬虫.py
@@ -9,43 +9,36 @@ def download_one_charcter(url):
     soup1 = soup.find_all('div', id='content')
     soup2 = soup.find_all('div', class_='p')
     for txt1 in soup1:
-        # print(txt1.text)
         txt11 = txt1.text
-        # print(txt11)
-    # print(soup1.text)
-    # print(soup2)
     for txt3 in soup2:
         txt4 = txt3.find_all('a')
         for txt5 in txt4[1:2]:
             title_1 = txt5.text
     for txt in txts[0:1]:
-        # print(txt.h1.text)
         title = txt.h1.text
-        # print(title)
         print('正在下载... ' + title)
         with open(title_1 + '.txt', 'a', encoding='utf-8') as f:
             f.write(title + '\n')
             f.write(txt11)
     pass
-bookmulu='http://www.shuquge.com/txt/8072/index.html'   #此处为要下载小说的目录链接
-r=requests.get(bookmulu)
-bookmulu_pre=bookmulu.replace('index.html','')
-r.encoding=r.apparent_encoding
-demo=r.text
-soup=BeautifulSoup(demo,'html.parser')
-one_titles=soup.find_all("div",class_="listmain")
-# print(one_titles)
-for item in one_titles:
-    txts=item.find_all('a')
-    # print(item)
-    # print(txts)
-    for tag_a in txts[12:]:
-        # print(tag_a)
-        one_url=tag_a.get('href')
-        # print(one_url)
-        # one_title=tag_a.text
-        # print(one_title)
-        url=(bookmulu_pre+one_url)
-        # print(url)
-        download_one_charcter(url)
+def download_one_book(bookmulu):
+    r=requests.get(bookmulu)
+    bookmulu_pre=bookmulu.replace('index.html','')
+    r.encoding=r.apparent_encoding
+    demo=r.text
+    soup=BeautifulSoup(demo,'html.parser')
+    one_titles=soup.find_all("div",class_="listmain")
+    for item in one_titles:
+        txts=item.find_all('a')
+        for tag_a in txts[12:]:
+            one_url=tag_a.get('href')
+            url=(bookmulu_pre+one_url)
+            download_one_charcter(url)
+    pass
+def main():
+    bookmulu = 'http://www.shuquge.com/txt/8072/index.html'  # 此处为要下载小说的目录链接
+    download_one_book(bookmulu)
+    pass
+if __name__ =='__main__':
+    main()
 
