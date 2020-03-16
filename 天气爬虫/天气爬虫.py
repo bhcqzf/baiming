2cdadb250456f073830f18884584331daac32ac1
bai4246464@gmail.com <Bai:000123>
2020-03-17 07:45:20 +0800

    原版，加了随机数

--- 天气爬虫/天气爬虫.py
+++ 天气爬虫/天气爬虫.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import _random
 api="101090506"
 r = requests.get('http://t.weather.sojson.com/api/weather/city/'+api)
 r.encoding = r.apparent_encoding
@@ -16,6 +17,7 @@ week=forecastdata["week"]
 typea=forecastdata["type"]
 fx=forecastdata["fx"]
 fl=forecastdata["fl"]
+ra=_random.Random(1,20)
 print("早上好，我是你爹啊~~")
 print("爹猜你肯定没看天气预报")
 print("所以现在爹给你播报天气")
@@ -23,4 +25,4 @@ print("你现在处在的位置是：憋屈的"+shi+city)
 print("现在时间为："+date+" "+week)
 print("今天的天气是："+typea+" "+fx+fl+" "+high+" "+low+" 天气指数："+quality)
 print("您的亲爹真诚的提醒您，今天也要元气满满哦！")
-
+print(ra)
